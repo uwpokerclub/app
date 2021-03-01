64bedcae55dce20bece71008b97c282a6250b774
d2kalra <d2kalra@uwaterloo.ca>
2021-02-28 20:51:43 -0500

    events pages functionality WIP

--- src/pages/events/EventShow.jsx
+++ src/pages/events/EventShow.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Link, Switch, Route, useRouteMatch, useParams } from "react-router-dom";
 
@@ -7,94 +7,317 @@ import "./Events.scss";
 import EventSignIn from "./EventSignIn";
 
 export default function EventShow() {
+  const [isLoading, setIsLoading] = useState(true);
   const { path, url } = useRouteMatch();
   const { event_id } = useParams();
-  const event = {
-    "id": event_id,
-    "name": `Event ${event_id}`,
-    "format": "NLHE",
-    "start_date": new Date("2020-02-01T19:00:00"),
-    "notes": "First testing",
-    "count": 3,
-    "state": 0
+  const [event, setEvent] = useState({
+    id: event_id,
+    name: "",
+    start_date: "",
+    format: "",
+    notes: "",
+    semester_id: "",
+    state: 0
+  });
+  const [participants, setParticipants] = useState([]);
+  const [filteredParticipants, setFilteredParticipants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const [error, setError] = useState("");
+
+  const searchParticipants = (e) => {
+    setSearch(e.target.value);
+    e.preventDefault();
+
+    setFilteredParticipants(participants.filter((participant) => 
+      participant.first_name.toLowerCase().includes(search)
+      || participant.last_name.toLowerCase().includes(search)
+    ));
   };
-  const error = "";
-  const participants = [
-    {
-      "id": 0,
-      "first_name": "Bob",
-      "last_name": "Johnson",
-      "signed_out_at": new Date("2020-02-01T19:00:00"),
-      "placement": "--",
-      "event_id": 1
-    },
-    {
-      "id": 1,
-      "first_name": "Deep",
-      "last_name": "Kalra",
-      "signed_out_at": null,
-      "placement": "--",
-      "event_id": 1
-    },
-    {
-      "id": 2,
-      "first_name": "Adam",
-      "last_name": "Mahood",
-      "signed_out_at": new Date("2020-02-01T19:00:00"),
-      "placement": "--",
-      "event_id": 1
-    },
-    {
-      "id": 3,
-      "first_name": "Sasha",
-      "last_name": "Nayar",
-      "signed_out_at": null,
-      "placement": "--",
-      "event_id": 1
-    },
-    {
-      "id": 4,
-      "first_name": "Arham",
-      "last_name": "Abidi",
-      "signed_out_at": null,
-      "placement": "--",
-      "event_id": 1
+
+  const updateParticipants = (result) => {
+    if (result.status !== 200) {
+      setError(result.status);
     }
-  ];
+    else {
+      setError("");
+    }
+
+    fetch(`/api/participants/?eventId=${event_id}`)
+      .then((res) => res.json())
+      .then((data) => setParticipants(data.participants));
+  };
+  
+  const endEvent =  async () => {
+    const result = await fetch(`/api/events/${event_id}/end`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    if (result.status !== 200) {
+      setError(result.status);
+    }
+    else {
+      setError("");
+      setEvent({
+        id: event_id,
+        name: event.name,
+        start_date: event.start_date,
+        format: event.format,
+        notes: event.notes,
+        semester_id: event.semester_id,
+        state: 1
+      });
+    }
+  };
+
+  useEffect(() => {
+    const requests = [];
+
+    requests.push(fetch(`/api/events/${event_id}`).then((res) => res.json()));
+    requests.push(fetch(`/api/participants/?eventId=${event_id}`).then((res) => res.json()));
+
+    Promise.all(requests).then(([eventData, participantsData]) => {
+      setEvent(eventData.event);
+      setParticipants(participantsData.participants);
+      setFilteredParticipants(participantsData.participants);
+      setIsLoading(false);
+    });
+
+  }, [event_id]);
+
+  const EventTable = ({ participantsList, state }) => {
+    return (
+      <div className="panel panel-default">
+  
+        <div className="panel-heading">
+          <strong>Registered Entries </strong>
+          <span className="spaced faded">{participantsList.length}</span>
+        </div>
+  
+        <div className="list-registered">
+          <input 
+            type="text" 
+            placeholder="Find entry..." 
+            className="form-control search" 
+            value = {search} 
+            onChange={searchParticipants} />
+          <table className="table">
+  
+            <thead>
+              <tr>
+                <th>
+                  #
+                </th>
+                <th>
+                  First Name
+                </th>
+                <th>
+                  Last Name
+                </th>
+                <th>
+                  Student Number
+                </th>
+                <th>
+                  Signed Out At
+                </th>
+                <th className="center">
+                  Place
+                </th>
+                <th className="center">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+  
+            <tbody className="list">
+              {participantsList.map((participant, index) => (
+                <Participant participant={participant} index={index} state={state} />
+              ))}
+            </tbody>
+  
+          </table>
+        </div>
+  
+      </div>
+    );
+  };
+
+  const Participant = ({ participant, index, state }) => {
+  
+    const signOutParticipant = async (e, user_id) => {
+      e.preventDefault();
+  
+      const res = await fetch("/api/participants/sign-out", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          user_id,
+          event_id
+        })
+      });
+
+      updateParticipants(res);
+    };
+    
+    const signBackInParticipant = async (e, user_id) => {
+      e.preventDefault();
+  
+      const res = await fetch("/api/participants/sign-in", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          user_id,
+          event_id
+        })
+      });
+
+      updateParticipants(res);
+    };
+    
+    const removeParticipant = async (e, user_id) => {
+      e.preventDefault();
+  
+      const res = await fetch("/api/participants/delete", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          user_id,
+          event_id
+        })
+      });
+
+      updateParticipants(res);
+    };
+  
+    return (
+      <tr>
+  
+        <th>
+          {index + 1}
+        </th>
+  
+        <td className="fname">
+          {participant.first_name}
+        </td>
+  
+        <td className="lname">
+          {participant.last_name}
+        </td>
+  
+        <td className="studentno">
+          {participant.id}
+        </td>
+  
+        <td className="signed_out_at">
+          {
+            participant.signed_out_at
+            ? <span>{participant.signed_out_at.toLocaleString("en-US", { hour12: true, month: "short", day: "numeric", year: "numeric", hour: "numeric", minute: "2-digit" })}</span>
+            : <i>Not Signed Out</i>
+          }
+        </td>
+  
+        <td className="placement">
+          <span className="margin-center">
+            {
+              participant.placement
+              ? participant.placement
+              : "--"
+            }
+          </span>
+        </td>
+  
+        <td className="center">
+          {state !== 1 &&
+            <div className="btn-group">
+              {
+                !participant.signed_out_at
+                ?
+                  <form onSubmit={signOutParticipant} className="form-inline">
+  
+                    <div className="form-group">
+                      <input type="hidden" name="user_id" value={participant.id} className="form-control" />
+                    </div>
+  
+                    <button type="submit" className="btn btn-info">
+                      Sign Out
+                    </button>
+  
+                  </form>
+                :
+                  <form onSubmit={signBackInParticipant} className="form-inline">
+  
+                  <div className="form-group">
+                    <input type="hidden" name="user_id" value={participant.id} className="form-control" />
+                  </div>
+  
+                  <button type="submit" className="btn btn-primary">
+                  Sign Back In
+                  </button>
+  
+                  </form>
+              }
+              <form onSubmit={removeParticipant} className="form-inline">
+  
+                <div className="form-group">
+                  <input type="hidden" name="user_id" value={participant.id} className="form-control" />
+                </div>
+  
+                <button type="submit" className="btn btn-warning">
+                  Remove
+                </button>
+  
+              </form>
+            </div>
+          }
+        </td>
+  
+      </tr>
+    );
+  };
 
   return (
     <Switch>
       <Route exact path={path}>
-        <div>
-          {event.state === 1 &&
-            <div className="alert alert-danger">This event has ended.</div>
-          }
+        {!isLoading && (
+          <div>
+            {event.state === 1 &&
+              <div className="alert alert-danger">This event has ended.</div>
+            }
 
-          {error &&
-            <div className="alert alert-danger">error</div>
-          }
+            {error &&
+              <div className="alert alert-danger">error</div>
+            }
 
-          <h1>{event.name}</h1>
-          <p><strong>Format:</strong> {event.format}</p>
-          <p><strong>Date:</strong> {event.start_date.toLocaleString("en-US", { hour12: true, month: "short", day: "numeric", year: "numeric", hour: "numeric", minute: "2-digit" })}</p>
-          <p><strong>Additional Details:</strong> {event.notes}</p>
+            <h1>{event.name}</h1>
+            <p><strong>Format:</strong> {event.format}</p>
+            <p><strong>Date:</strong> {event.start_date.toLocaleString("en-US", { hour12: true, month: "short", day: "numeric", year: "numeric", hour: "numeric", minute: "2-digit" })}</p>
+            <p><strong>Additional Details:</strong> {event.notes}</p>
 
-          {event.state !== 1 &&
-            <div className="Button__group">
+            {event.state !== 1 &&
+              <div className="Button__group">
 
-              <Link to={`${url}/sign-in`} className="btn btn-primary">Register Members</Link>
+                <Link to={`${url}/sign-in`} className="btn btn-primary">Register Members</Link>
 
-              <form onSubmit={endEvent}>
-                <input type="hidden" name="event_id" value={event_id} />
-                <button type="submit" className="btn btn-danger">End Event</button>
-              </form>
+                <form onSubmit={endEvent}>
+                  <button type="submit" className="btn btn-danger">End Event</button>
+                </form>
 
-            </div>
-          }
+              </div>
+            }
 
-          <EventTable participants={participants} state={event.state} />
+            <EventTable participantsList={filteredParticipants} state={event.state} />
 
-        </div>
+          </div>
+        )}
       </Route>
       <Route exact path={`${path}/sign-in`}>
         <EventSignIn />
@@ -102,170 +325,3 @@ export default function EventShow() {
     </Switch>
   );
 }
-
-const EventTable = ({ participants, state }) => {
-  return (
-    <div className="panel panel-default">
-
-      <div className="panel-heading">
-        <strong>Registered Entries </strong>
-        <span className="spaced faded">{participants.length}</span>
-      </div>
-
-      <div className="list-registered">
-        <input type="text" placeholder="Find entry..." className="form-control search" />
-        <table className="table">
-
-          <thead>
-            <tr>
-              <th>
-                #
-              </th>
-              <th>
-                First Name
-              </th>
-              <th>
-                Last Name
-              </th>
-              <th>
-                Student Number
-              </th>
-              <th>
-                Signed Out At
-              </th>
-              <th className="center">
-                Place
-              </th>
-              <th className="center">
-                Actions
-              </th>
-            </tr>
-          </thead>
-
-          <tbody className="list">
-            {participants.map((participant, index) => (
-              <Participant participant={participant} index={index} state={state} />
-            ))}
-          </tbody>
-
-        </table>
-      </div>
-
-    </div>
-  );
-};
-
-const Participant = ({ participant, index, state }) => {
-  return (
-    <tr>
-
-      <th>
-        {index + 1}
-      </th>
-
-      <td className="fname">
-        {participant.first_name}
-      </td>
-
-      <td className="lname">
-        {participant.last_name}
-      </td>
-
-      <td className="studentno">
-        {participant.id}
-      </td>
-
-      <td className="signed_out_at">
-        {
-          participant.signed_out_at
-          ? <span>{participant.signed_out_at.toLocaleString("en-US", { hour12: true, month: "short", day: "numeric", year: "numeric", hour: "numeric", minute: "2-digit" })}</span>
-          : <i>Not Signed Out</i>
-        }
-      </td>
-
-      <td className="placement">
-        <span className="margin-center">
-          {
-            participant.placement
-            ? participant.placement
-            : "--"
-          }
-        </span>
-      </td>
-
-      <td className="center">
-        {state !== 1 &&
-          <div className="btn-group">
-            {
-              !participant.signed_out_at
-              ?
-                <form onSubmit={signOutParticipant} className="form-inline">
-
-                  <div className="form-group">
-                    <input type="hidden" name="user_id" value={participant.id} className="form-control" />
-                  </div>
-
-                  <div className="form-group">
-                    <input type="hidden" name="event_id" value={participant.event_id} className="form-control" />
-                  </div>
-
-                  <button type="submit" className="btn btn-info">
-                    Sign Out
-                  </button>
-
-                </form>
-              :
-                <form onSubmit={signBackInParticipant} className="form-inline">
-
-                <div className="form-group">
-                  <input type="hidden" name="user_id" value={participant.id} className="form-control" />
-                </div>
-
-                <div className="form-group">
-                  <input type="hidden" name="event_id" value={participant.event_id} className="form-control" />
-                </div>
-
-                <button type="submit" className="btn btn-primary">
-                Sign Back In
-                </button>
-
-                </form>
-            }
-            <form onSubmit={removeParticipant} className="form-inline">
-
-              <div className="form-group">
-                <input type="hidden" name="user_id" value={participant.id} className="form-control" />
-              </div>
-
-              <div className="form-group">
-                <input type="hidden" name="event_id" value={participant.event_id} className="form-control" />
-              </div>
-
-              <button type="submit" className="btn btn-warning">
-                Remove
-              </button>
-
-            </form>
-          </div>
-        }
-      </td>
-
-    </tr>
-  );
-};
-
-const signOutParticipant = (user_id, event_id) => {
-  return ;
-};
-
-const signBackInParticipant = (user_id, event_id) => {
-  return ;
-};
-
-const removeParticipant = (user_id, event_id) => {
-  return ;
-};
-
-const endEvent = (event_id) => {
-  return ;
-};
--- src/pages/events/EventSignIn.jsx
+++ src/pages/events/EventSignIn.jsx
@@ -1,97 +1,136 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from "react-router-dom";
 
 import { useParams } from "react-router-dom";
 
 import "./Events.scss";
 
 export default function EventSignIn() {
+  const history = useHistory();
   const { event_id } = useParams();
-  const members = [
-    {
-      "id": 0,
-      "first_name": "Bob",
-      "last_name": "Johnson"
-    },
-    {
-      "id": 1,
-      "first_name": "Deep",
-      "last_name": "Kalra"
-    },
-    {
-      "id": 2,
-      "first_name": "Adam",
-      "last_name": "Mahood"
-    },
-    {
-      "id": 3,
-      "first_name": "Sasha",
-      "last_name": "Nayar"
-    },
-    {
-      "id": 4,
-      "first_name": "Arham",
-      "last_name": "Abidi"
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [participants, setParticipants] = useState([]);
+  const [semesterId, setSemesterId] = useState("");
+  const [members, setMembers] = useState([]);
+  const [selectedMembers, setSelectedMembers] = useState([]);
+  const [isChecked, setIsChecked] = useState(false);
+
+  const toggleMember = (e) => {
+    e.preventDefault();
+
+    if (e.target.checked) {
+      setSelectedMembers(selectedMembers.push(e.target.value));
+    } else {
+      setSelectedMembers(selectedMembers.filter(
+        member => member !== e.target.value
+      ));
     }
-  ];
+  };
+
+  const registerMembersForEvent = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch("/api/participants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        event_id,
+        selectedMembers
+      })
+    });
+
+    if (res.status === 201) {
+      return history.push(`/events/${event_id}`);
+    }
+  };
+
+  useEffect(() => {
+    const requests = [];
+
+    requests.push(fetch(`/api/events/${event_id}`).then((res) => res.json()));
+    requests.push(fetch(`/api/participants/?eventId=${event_id}`).then((res) => res.json()));
+
+    Promise.all(requests).then(([eventData, participantsData]) => {
+      setSemesterId(eventData.event.semester_id);
+      setParticipants(participantsData.participants.map((participant) => participant.user_id));
+    });
+
+    const membersRequest = [
+      fetch(`/api/users?semesterId=${semesterId}`).then((res) => res.json())
+    ];
+
+    Promise.all(membersRequest).then(([membersData]) => {
+      setMembers(membersData.users.filter((user) => participants.indexOf(user.id) === -1));
+      setIsLoading(false);
+    });
+    //onClick={setIsChecked(!isChecked)}
+  }, [event_id, participants, semesterId]);
+
+  const Member = ({ member }) => {
+    return (
+      <div className="Participants__item">
+  
+        <div className="Participants__item-checkbox">
+          <input 
+            type="checkbox" 
+            name="selected" 
+            value={member.id}
+            checked={isChecked}
+             />
+        </div>
+  
+        <div className="Participants__item-title">
+          <span>
+            {member.first_name} {member.last_name}
+          </span>
+        </div>
+  
+        <div className="Participants__item-student_id">
+          <span>
+            {member.id}
+          </span>
+        </div>
+  
+      </div>
+    );
+  };
 
   return (
     <div className="row">
-      <div className="col-md-3" />
-      <div className="col-md-6">
-        <div className="Participants">
+      {!isLoading && (
+        <>
+          <div className="col-md-3" />
+          <div className="col-md-6">
+            <div className="Participants">
 
-          <h3 className="center bold">
-            Sign In Members
-          </h3>
+              <h3 className="center bold">
+                Sign In Members
+              </h3>
 
-          <form onSubmit={registerMembersForEvent}>
+              <form onSubmit={registerMembersForEvent}>
 
-            <input type="hidden" name="event_id" value={event_id} />
+                {members.map((member) => (
+                  <Member member={member} />
+                ))}
 
-            {members.map((member) => (
-              <Member member={member} />
-            ))}
+                <div className="Participants__submit">
+                  <div className="row">
+                    <button type="submit" className="mx-auto btn btn-primary">
+                      Sign In
+                    </button>
+                  </div>
+                </div>
 
-            <div className="Participants__submit">
-              <div className="row">
-                <button type="submit" className="mx-auto btn btn-primary">
-                  Sign In
-                </button>
-              </div>
+              </form>
+
             </div>
-
-          </form>
-
-        </div>
-      </div>
-      <div className="col-md-3" />
+          </div>
+          <div className="col-md-3" />
+        </>
+      )}
     </div>
   );
 }
-
-const Member = ({ member }) => {
-  return (
-    <div className="Participants__item">
-
-      <div className="Participants__item-checkbox">
-        <input type="checkbox" name="selected" value={member.id} />
-      </div>
-
-      <div className="Participants__item-title">
-        <span>
-          {member.first_name} {member.last_name}
-        </span>
-      </div>
-
-      <div className="Participants__item-student_id">
-        <span>
-          {member.id}
-        </span>
-      </div>
-
-    </div>
-  );
-};
-
-const registerMembersForEvent = (event_id) => {
-};
